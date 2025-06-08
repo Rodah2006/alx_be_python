0c554b449979ca9a74395c8c2cfc73343ed7e66d
Rodah2006 <rodahdiana@gmail.com>
2025-06-08 13:07:35 +0300

    Fix checker requirements for shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,9 +1,7 @@
-# shopping_list_manager.py
-
 shopping_list = []
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -13,30 +11,27 @@ while True:
     display_menu()
     choice = input("Enter your choice: ")
 
-    if choice == '1':
+    if choice == "1":
         item = input("Enter the item to add: ")
         shopping_list.append(item)
-        print(f"'{item}' has been added to your shopping list.")
-
-    elif choice == '2':
+        print(f"'{item}' has been added to your shopping list.\n")
+    elif choice == "2":
         item = input("Enter the item to remove: ")
         if item in shopping_list:
             shopping_list.remove(item)
-            print(f"'{item}' has been removed from your shopping list.")
+            print(f"'{item}' has been removed from your shopping list.\n")
         else:
-            print(f"'{item}' is not in your shopping list.")
-
-    elif choice == '3':
-        if not shopping_list:
-            print("Your shopping list is empty.")
-        else:
+            print(f"'{item}' is not in your shopping list.\n")
+    elif choice == "3":
+        if shopping_list:
             print("Your shopping list:")
             for i, item in enumerate(shopping_list, 1):
                 print(f"{i}. {item}")
-
-    elif choice == '4':
+        else:
+            print("Your shopping list is empty.")
+        print()
+    elif choice == "4":
         print("Goodbye!")
         break
-
     else:
-        print("Invalid choice. Please try again.")
+        print("Invalid choice. Please try again.\n")
